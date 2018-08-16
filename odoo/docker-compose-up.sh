4d370b0f4043100e0c2ae7a64172c40ec1834f83
likuan <likuan33@126.com>
2018-08-16 12:44:09 +0800

    odoo

---
+++ odoo/docker-compose-up.sh
@@ -0,0 +1,11 @@
+
+scp docker-compose.yml root@114.116.28.26:/root
+ssh root@114.116.28.26
+
+docker-compose -f docker-compose.yml up
+docker-compose -f docker-compose.yml down
+
+docker-compose -f docker-compose.yml stop
+docker-compose -f docker-compose.yml start
+
+http://114.116.28.26:8069
